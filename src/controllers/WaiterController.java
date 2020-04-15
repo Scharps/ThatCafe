db7ea7fc9268abc175cc1f794496af6dd2d3eab7
ashley <ashleyforster@blueyonder.co.uk>
2020-04-15 15:34:39 +0100

    added specials tab to all menus in customer and Waiter added initialise menu method

--- src/controllers/WaiterController.java
+++ src/controllers/WaiterController.java
@@ -29,12 +29,15 @@ import java.util.Calendar;
 import java.util.ResourceBundle;
 
 public class WaiterController implements Initializable {
-    @FXML private TableView<MenuItem> foodtable;
-    @FXML private TableColumn<MenuItem, String> foodname;
-    @FXML private TableColumn<MenuItem, Double> foodprice;
-    @FXML private TableView<MenuItem> drinktable;
-    @FXML private TableColumn<MenuItem, String> drinkname;
-    @FXML private TableColumn<MenuItem, Double> drinkprice;
+    @FXML private TableView<MenuItem> foodTable;
+    @FXML private TableColumn<MenuItem, String> foodName;
+    @FXML private TableColumn<MenuItem, Double> foodPrice;
+    @FXML private TableView<MenuItem> drinkTable;
+    @FXML private TableColumn<MenuItem, String> drinkName;
+    @FXML private TableColumn<MenuItem, Double> drinkPrice;
+    @FXML private TableView<MenuItem> specialsTable;
+    @FXML private TableColumn<MenuItem, String> specialsName;
+    @FXML private TableColumn<MenuItem, Double> specialsPrice;
     @FXML private TableView<MenuItem> ordertable;
     @FXML private TableColumn<MenuItem, String> ordereditem;
     @FXML private ComboBox<Integer> tableno;
@@ -44,6 +47,7 @@ public class WaiterController implements Initializable {
 
     private ObservableList<MenuItem> fooditems = FXCollections.observableArrayList();
     private ObservableList<MenuItem> orderitems = FXCollections.observableArrayList();
+    private ObservableList<MenuItem> specialitems = FXCollections.observableArrayList();
     private ObservableList<Integer> tables = FXCollections.observableArrayList();
     private ObservableList<MenuItem> drinksitems = FXCollections.observableArrayList();
 
@@ -58,14 +62,21 @@ public class WaiterController implements Initializable {
     }
 
     public void foodSelect(MouseEvent event){
-        MenuItem itemSelected = foodtable.getSelectionModel().getSelectedItem();
+        MenuItem itemSelected = foodTable.getSelectionModel().getSelectedItem();
         orderitems.add(itemSelected);
         ordereditem.setCellValueFactory((new PropertyValueFactory<MenuItem, String>("name")));
         ordertable.setItems(orderitems);
     }
 
     public void drinkSelect(MouseEvent event){
-        MenuItem itemSelected = drinktable.getSelectionModel().getSelectedItem();
+        MenuItem itemSelected = drinkTable.getSelectionModel().getSelectedItem();
+        orderitems.add(itemSelected);
+        ordereditem.setCellValueFactory((new PropertyValueFactory<MenuItem, String>("name")));
+        ordertable.setItems(orderitems);
+    }
+
+    public void specialSelect(MouseEvent event){
+        MenuItem itemSelected = specialsTable.getSelectionModel().getSelectedItem();
         orderitems.add(itemSelected);
         ordereditem.setCellValueFactory((new PropertyValueFactory<MenuItem, String>("name")));
         ordertable.setItems(orderitems);
@@ -117,29 +128,15 @@ public class WaiterController implements Initializable {
         AppState appState = AppState.getAppState();
         namelabel.setText(appState.getUser().getFirstName());
 
-        foodname.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
-        foodprice.setCellValueFactory(new PropertyValueFactory<MenuItem, Double>("price"));
-        foodtable.setItems(fooditems);
 
-        drinkname.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
-        drinkprice.setCellValueFactory(new PropertyValueFactory<MenuItem, Double>("price"));
-        drinktable.setItems(drinksitems);
 
         tableno.setItems(tables);
-
+        initialiseMenu();
 
         try {
             Connection conn = DatabaseService.getConnection();
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM MenuItems WHERE ItemType = 'Food'");
-            while(rs.next()){
-                fooditems.add(MenuItem.createMenuItem(rs.getInt(1), rs.getString(2), MenuItemType.Food, rs.getDouble(4), rs.getInt(5), rs.getBoolean(6)));
-            }
-            rs = st.executeQuery("SELECT * FROM MenuItems WHERE ItemType = 'Drink'");
-            while(rs.next()){
-                drinksitems.add(MenuItem.createMenuItem(rs.getInt(1), rs.getString(2), MenuItemType.Drink, rs.getDouble(4), rs.getInt(5), rs.getBoolean(6)));
-            }
-            rs = st.executeQuery("SELECT TableId from CafeTables");
+            ResultSet rs = st.executeQuery("SELECT TableId from CafeTables");
             while(rs.next()){
                 tables.add(rs.getInt(1));
             }
@@ -149,6 +146,41 @@ public class WaiterController implements Initializable {
         catch (SQLException se){
             se.printStackTrace();
             System.out.println(se);
+        }
+
+    }
+
+    public void initialiseMenu(){
+        fooditems.clear();
+        drinksitems.clear();
+        specialitems.clear();
+
+        foodName.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
+        foodPrice.setCellValueFactory(new PropertyValueFactory<MenuItem, Double>("price"));
+        foodTable.setItems(fooditems);
+        drinkName.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
+        drinkPrice.setCellValueFactory(new PropertyValueFactory<MenuItem, Double>("price"));
+        drinkTable.setItems(drinksitems);
+        specialsName.setCellValueFactory(new PropertyValueFactory<MenuItem, String>("name"));
+        specialsPrice.setCellValueFactory(new PropertyValueFactory<MenuItem, Double>("price"));
+        specialsTable.setItems(specialitems);
+        try{
+            Connection conn = DatabaseService.getConnection();
+            ResultSet rs = MenuItem.getMenuItems(conn, false);
+            while(rs.next()) {
+                if (rs.getString(3).equals(String.valueOf(MenuItemType.Food))){
+                    fooditems.add(MenuItem.createMenuItem(rs.getInt(1), rs.getString(2), MenuItemType.Food, rs.getDouble(4), rs.getInt(5), rs.getBoolean(6)));
+                } else {
+                    drinksitems.add(MenuItem.createMenuItem(rs.getInt(1), rs.getString(2), MenuItemType.Drink, rs.getDouble(4), rs.getInt(5), rs.getBoolean(6)));
+                }
+            }
+
+            rs = MenuItem.getMenuItems(conn, true);
+            while(rs.next()){
+                specialitems.add(MenuItem.createMenuItem(rs.getInt(1), rs.getString(2), MenuItemType.valueOf(rs.getString(3)), rs.getDouble(4), rs.getInt(5), rs.getBoolean(6)));
+            }
+            conn.close();
+        }catch (SQLException se){
         }
 
     }
