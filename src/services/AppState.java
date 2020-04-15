a258699e8c70588c36f4119a8b75a44d45d6709c
Sam J <507.s94j@gmail.com>
2020-04-15 11:42:50 +0100

    remove functionality

--- src/services/AppState.java
+++ src/services/AppState.java
@@ -2,23 +2,11 @@ package services;
 
 import models.User;
 
-import java.sql.Connection;
-
 public final class AppState {
 
     private static AppState appState_;
     private User user_;
-    private Connection conn;
     private AppState(){};
-
-    public Connection getConn() {
-        return conn;
-    }
-
-    public void setConn(Connection conn) {
-        this.conn = conn;
-    }
-
 
     public static AppState getAppState() {
         if(appState_ == null) {
