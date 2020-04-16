f5e73668bf9fef2af9696fc265b6bd33a9c14a10
Sam J <507.s94j@gmail.com>
2020-04-16 20:30:50 +0100

    Updated database

--- src/migration/Covid19Database.sql
+++ src/migration/Covid19Database.sql
@@ -7,18 +7,18 @@ CREATE TABLE Address(
     FirstLine varchar(255) not null,
     City varchar(255) not null,
     PostCode varchar(10) not null,
-Primary key(AddressId)
+    Primary key(AddressId)
 );
 
 Create Table Customers (
-    CustomerId Integer auto_increment,
-    Username varchar(255) not null unique,
-    Password varchar(255) not null unique,
-    FName varchar(255) not null,
+    CustomerId Integer auto_increment,  
+    Username varchar(255) not null unique,  
+    Password varchar(255) not null unique,  
+    FName varchar(255) not null,  
     LName varchar(255) not null,
-AddressId Integer not  null,
-Primary key (CustomerId),
-Foreign Key (AddressId) references Address(AddressId)
+    AddressId Integer not  null,  
+    Primary key (CustomerId),  
+    Foreign Key (AddressId) references Address(AddressId)
 );
 
 create table Rota(
@@ -37,7 +37,7 @@ create table Rota(
 	SatFinish VARCHAR(5) default "None",
 	SunStart VARCHAR(5) default "None",
 	SunFinish VARCHAR(5) default "None",
-Primary Key (RotaId)
+	Primary Key (RotaId)
 );
 
 Create Table Staff(
@@ -46,94 +46,100 @@ Create Table Staff(
     FName varchar(255) not null,
     LName varchar(255) not null,
     StaffPos varchar(10) not null,
-RotaId INTEGER not null,
-FOREIGN KEY (RotaId) REFERENCES Rota(RotaId),
-Primary key(StaffId)
+    RotaId INTEGER not null,
+    FOREIGN KEY (RotaId) REFERENCES Rota(RotaId),
+    Primary key(StaffId)
 );
 
 INSERT INTO Rota() VALUES();
 INSERT INTO Staff(Password,FName,LName,StaffPos,RotaId) values ("1234","Mr.","Manager","Manager",1);
 
 Create table CafeTables(
-  TableId Integer auto_increment,
-Capacity Integer not null,
-Primary Key(TableId)
+    TableId Integer auto_increment,
+    Capacity Integer not null,
+    Primary Key(TableId)
 );
 
 INSERT INTO CafeTables(Capacity) values (2),(2),(2),(2),(4),(4),(4),(4),(8),(8),(10);
 
-
 create table MenuItems(
-    ItemId integer auto_increment not null,
-    ItemName varchar(255) not null,
-ItemType enum('Food', 'Drink') not null,
-Price Decimal(4,2) not null,
-Sold Integer not null default 0,
-SpecialSpecial BOOLEAN DEFAULT FALSE,
-Primary key (ItemId)
+    ItemId integer auto_increment not null, 
+    ItemName varchar(255) not null, 
+    ItemType enum('Food', 'Drink') not null,
+    Price Decimal(4,2) not null, 
+    Sold Integer not null default 0,
+    Special BOOLEAN DEFAULT FALSE,
+    Primary key (ItemId)
 );
 
 INSERT INTO MenuItems(ItemName,ItemType,Price) VALUES ("Pizza", 'Food', 11.99), ('Beef Burger','Food', 8.99), ("Cola", 'Drink', 1.99), ("Beer", "Drink", 3.25);
 
 Create Table Orders(
-    OrderId Integer auto_increment not null,
+    OrderId Integer auto_increment not null,    
     OrderDate DATETIME not null,
-CustomerId Integer not null,
-Cooked Boolean not null default 0,
-OrderTotal Decimal(5,2) not null default 0,
-OrderType enum('Eatin','Takeaway','Delivery') not null,
-Primary key (OrderId),
-Foreign Key (CustomerId) references Customers(CustomerId)
+    CustomerId Integer not null,  
+    Cooked Boolean not null default 0,
+    OrderTotal Decimal(5,2) not null default 0,
+    Primary key (OrderId),  
+    Foreign Key (CustomerId) references Customers(CustomerId)
 );
 
 Create table OrderedItems(
 	OrderedItemID Integer auto_increment not null,
-OrderID Integer not null,
-ItemID Integer not null,
-Quantity INTEGER NOT NULL,
-Primary Key (OrderedItemID),
-Foreign Key (OrderID) references Orders(OrderID),
-Foreign Key (ItemID) references MenuItems(ItemID)
+	OrderID Integer not null,
+	ItemID Integer not null,
+	Quantity INTEGER NOT NULL,
+	Primary Key (OrderedItemID),
+	Foreign Key (OrderID) references Orders(OrderID),
+	Foreign Key (ItemID) references MenuItems(ItemID)
+); 
+    
+ Create Table Bookings ( 
+    BookingId Integer auto_increment not null UNIQUE,
+    TableId Integer not null, 
+    CustomerId Integer not null, 
+    BookingDate DATE not null,
+    BookingHour INT not null,
+    GuestQuantity Integer not null,
+    Approved boolean not null default 0,
+    PRIMARY KEY(TableId, BookingDate),
+    Foreign Key (TableId) references CafeTables(TableId), 
+    Foreign Key (CustomerId) references Customers(CustomerId)
 );
-
-Create Table Bookings (
-  BookingId Integer auto_increment not null UNIQUE,
-TableId Integer not null,
-CustomerId Integer not null,
-BookingDate DATE not null,
-BookingHour INT not null,
-GuestQuantity Integer not null,
-Approved boolean not null default 0,
-PRIMARY KEY(TableId, BookingDate),
-Foreign Key (TableId) references CafeTables(TableId),
-Foreign Key (CustomerId) references Customers(CustomerId)
-);
-
+    
 create table EatinOrders(
-  OrderID Integer,
-TableID Integer not null,
-Served Boolean default 0,
-Primary Key (OrderId),
-Foreign Key (OrderId) references Orders(OrderId),
-Foreign Key (TableId) references CafeTables(TableId)
+	OrderID Integer,
+	TableID Integer not null,
+	Primary Key (OrderId),
+	Foreign Key (OrderId) references Orders(OrderId),
+	Foreign Key (TableId) references CafeTables(TableId)
 );
 
 create table TakeawayOrders(
 	OrderID Integer,
 	PickUpTime DATETIME not null,
 	Collected Boolean not null default 0,
-Primary Key (OrderId),
-Foreign Key (OrderId) references Orders(OrderId)
+	Primary Key (OrderId),
+	Foreign Key (OrderId) references Orders(OrderId)
 );
 
 create table DeliveryOrders(
 	OrderId Integer,
 	Confirmed Boolean not null default 0,
 	DeliveryTime DATETIME not null,
-DriverId Integer,
-Delivered Boolean not null default 0,
-Primary Key (OrderId),
-Foreign Key (OrderId) references Orders(OrderId),
-Foreign Key (DriverId) references Staff(StaffId)
+	DriverId Integer,
+	Delivered Boolean not null default 0,
+	Primary Key (OrderId),
+	Foreign Key (OrderId) references Orders(OrderId),
+	Foreign Key (DriverId) references Staff(StaffId)
+);
+
+CREATE TABLE HoursWorked (
+	HoursWorkedId INTEGER,
+    StaffId INTEGER NOT NULL,
+    DateWorked DATE NOT NULL,
+    WorkedHours INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (HoursWorkedId),
+    FOREIGN KEY (StaffId) REFERENCES Staff(StaffId)
 );
     
